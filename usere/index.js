fc1775a0b2073a2ae2caca5aaa661258d286ade2
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2024-08-27 18:11:31 -0300

    more accs

---
+++ usere/index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+
+const packageDefinition = protoLoader.loadSync(
+  path.resolve(__dirname, "src", "pb", "messages.proto"),
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  }
+);
+
+const proto = grpc.loadPackageDefinition(packageDefinition);
